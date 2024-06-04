1bae4cbbf649023cf67ae3f0a5359fe49d4c06ac
the-nazzmul <nazmul.hussain.utchash@gmail.com>
2024-06-04 09:26:19 +0600

    connection

---
+++ src/components/course/Image-form.tsx
@@ -0,0 +1,120 @@
+"use client";
+
+import axios from "axios";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { ImageIcon, Pencil, PlusCircle } from "lucide-react";
+import { useState } from "react";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
+import { cn } from "@/lib/utils";
+import { Textarea } from "../ui/textarea";
+import Image from "next/image";
+import { FileUpload } from "../file-upload";
+
+interface IImageForm {
+  initialData: {
+    imageUrl: string | null;
+  };
+  courseId: string;
+}
+
+const formSchema = z.object({
+  imageUrl: z.string().min(1, { message: "Image is required" }),
+});
+
+const ImageForm = ({ initialData, courseId }: IImageForm) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const router = useRouter();
+
+  const toggleEdit = () => {
+    setIsEditing((current) => !current);
+  };
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      imageUrl: initialData.imageUrl || "",
+    },
+  });
+
+  const { isSubmitting, isValid } = form.formState;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      await axios.patch(`/api/courses/${courseId}`, values);
+      toast.success("Course updated!");
+      toggleEdit();
+      router.refresh();
+    } catch {
+      toast.error("Something went wrong");
+    }
+  };
+
+  return (
+    <div className="mt-6 border bg-slate-100 rounded-md p-4">
+      <div className="font-medium flex items-center justify-between">
+        Course image
+        <Button onClick={toggleEdit} variant="ghost">
+          {isEditing && <>Cancel</>}
+          {!isEditing && !initialData.imageUrl && (
+            <>
+              <PlusCircle className="h-4 w-4 mr-2" />
+              Add an image
+            </>
+          )}
+          {!isEditing && initialData.imageUrl && (
+            <>
+              <Pencil className="h-4 w-4 mr-2" />
+              Edit image
+            </>
+          )}
+        </Button>
+      </div>
+      {!isEditing &&
+        (!initialData.imageUrl ? (
+          <div className="flex items-center justify-center h-60 bg-slate-200 rounded-md">
+            <ImageIcon className="h-10 w-10 text-slate-500" />
+          </div>
+        ) : (
+          <div className="relative aspect-video mt-2">
+            <Image
+              src={initialData.imageUrl}
+              alt="Upload"
+              fill
+              className="object-cover rounded-md"
+            />
+          </div>
+        ))}
+      {isEditing && (
+        <div>
+          <FileUpload
+            endpoint="courseImage"
+            onChange={(url) => {
+              if (url) {
+                onSubmit({ imageUrl: url });
+              }
+            }}
+          />
+          <div className="text-xs text-muted-foreground mt-4">
+            16:9 aspect ratio recommended
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ImageForm;
---
+++ src/components/ui/navbar/Navbar.tsx
@@ -0,0 +1,13 @@
+import MobileSidebar from "./MobileSidebar";
+import NavbarRoutes from "./Nabvar-routes";
+
+const Navbar = () => {
+  return (
+    <div className="p-4 border-b h-full flex items-center bg-white shadow-sm ">
+      <MobileSidebar />
+      <NavbarRoutes />
+    </div>
+  );
+};
+
+export default Navbar;
